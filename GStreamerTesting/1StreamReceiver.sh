07cb5956564c01df9eaecdbf5e1e638a8d4e47a2
Jaden Jovan <jaden.jov@gmail.com>
2024-06-28 12:08:01 -0500

    fix receiver

--- GStreamerTesting/1StreamReceiver.sh
+++ GStreamerTesting/1StreamReceiver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gst-launch-1.0 udpsrc address=172.17.141.191 port=5000 caps=application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! autovideosink
+gst-launch-1.0 udpsrc port=5000 caps=application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! autovideosink
