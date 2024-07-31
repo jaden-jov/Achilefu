6195e1d79b9e4c0039c688ab662daec75d1120fa
Jaden Jovan <jaden.jov@gmail.com>
2024-07-31 12:08:51 -0500

    wrote inital gsttrans test

--- Stereoscopic/DisplayTesting/gstTransTest.cpp
+++ Stereoscopic/DisplayTesting/gstTransTest.cpp
@@ -5,9 +5,19 @@ using namespace cv;
 using namespace std;
 
 int main(){
+    // Define pipelines
     VideoCapture cap("gst-launch-1.0 filesrc location=./image1.jpg ! jpegdec ! videoconvert ! video/x-raw, format=RGB ! alpha alpha=1.0 ! video/x-raw, format=RGBA ! appsink", CAP_GSTREAMER);
     VideoWriter out("appsrc ! videoconvert ! video/x-raw, format=RGBA ! kmssink ")
+    
+    // Define variables
     Mat frame;
+    Size newSize(1920, 1080);
+    Mat newframe;
+
+    // Read and resize frame
     cap.read(frame);
-    out.write(frame);
+    resize(frame, newframe, newSize);
+
+    // Display
+    out.write(newframe);
 }
